bab60409238a3b91569b254cd281944e0e461f23
Alex Bulankou <albulank@microsoft.com>
2017-04-21 19:27:10 +0200

    Backend service

---
+++ src/StockTracker.Backend/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿namespace StockTracker.Backend.Controllers
+{
+    using System.IO;
+    using System.Net;
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ContentResult Stock(string symbol)
+        {
+            var result = string.Empty;
+            HttpWebRequest httpClient = (HttpWebRequest)HttpWebRequest.Create("http://finance.google.com/finance/info?client=ig&q=" + symbol);
+            using (StreamReader reader = new StreamReader(httpClient.GetResponse().GetResponseStream()))
+            {
+                result = reader.ReadToEnd();
+            }
+            return Content(result.Substring(3), "application/json");
+        }
+    }
+}
